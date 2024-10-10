a441cb349a518cff6fdcac0101d86bb87ba297b4
alfinhimawan <alfinhimawan99@gmail.com>
2024-10-10 23:57:16 +0700

    Deskripsi perubahan

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        // sticky navbar on scroll script
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-    });
+$(document).ready(function () {
+  $(window).scroll(function () {
+    // sticky navbar on scroll script
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+  });
 });
+
